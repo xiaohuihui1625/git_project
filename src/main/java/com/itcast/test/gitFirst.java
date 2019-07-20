2c0437362453d8b2b8b27c5dd7df5f8674523f9b
xiaohuihui <xiaohuihui@itcast.cn>
2019-07-20 19:08:20 +0800

    增加hellogit

---
+++ src/main/java/com/itcast/test/gitFirst.java
@@ -0,0 +1,8 @@
+package com.itcast.test;
+
+public class gitFirst {
+    public static void main(String[] args) {
+        System.out.println("hello_git");
+        System.out.println("测试将项目导入到本地仓库中");
+    }
+}
